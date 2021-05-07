e096224936eac5ccdce6ff16a834210ee8ecdbfe
BB <2b@yorha.xyz>
2021-05-07 18:19:57 +0900

    增加代码可读性 (#5)

--- gotree.go
+++ gotree.go
@@ -26,9 +26,9 @@ const (
 )
 
 type FileTree struct {
-	level    int
-	fileType FileType
-	name     string
+	level    int      // 层级
+	fileType FileType // 类型
+	name     string   // 名称
 }
 
 func Print(path string) {
@@ -39,96 +39,96 @@ func Print(path string) {
 }
 
 func printDirs(path string) error {
-	list, err := readDir(path, 0)
+	files, err := readDir(path, 0)
 	if err != nil {
 		return err
 	}
 	var tabArray []string
-	for index, uri := range list {
+	for index, file := range files {
 		tab := ""
 		isLast := true
-		// 判断是否为当前文件将中最后一个
-		for i := index + 1; i < len(list); i++ {
-			if list[i].level < list[index].level {
+		// 判断是否为当前文件夹中最后一个
+		for i := index + 1; i < len(files); i++ {
+			if files[i].level < files[index].level {
 				break
 			}
-			if list[i].level == list[index].level {
+			if files[i].level == files[index].level {
 				isLast = false
 				break
 			}
 		}
 		// 删除多余的制表符
-		if index > 0 && len(tabArray) > 0 && list[index-1].level > list[index].level {
-			tabArray = tabArray[:uri.level]
+		if index > 0 && len(tabArray) > 0 && files[index-1].level > files[index].level {
+			tabArray = tabArray[:file.level]
 		}
 		// 添加对应的制表符
-		if isLast && uri.fileType != GeneralFile {
-			if len(tabArray) == uri.level {
+		if isLast && file.fileType != GeneralFile {
+			if len(tabArray) == file.level {
 				tabArray = append(tabArray, "  ")
 			}
-		} else if uri.fileType != GeneralFile {
-			if len(tabArray) == uri.level {
+		} else if file.fileType != GeneralFile {
+			if len(tabArray) == file.level {
 				tabArray = append(tabArray, "┃ ")
 			}
 		}
-		for i := 0; i < uri.level; i++ {
+		for i := 0; i < file.level; i++ {
 			tab = tab + tabArray[i]
 		}
-		if index == len(list)-1 || list[index].level > list[index+1].level || isLast {
+		if index == len(files)-1 || files[index].level > files[index+1].level || isLast {
 			tab = tab + "┗━"
 		} else {
 			tab = tab + "┣━"
 		}
-		fmt.Println(tab + print(uri.fileType)(uri.name))
+		fmt.Println(tab + print(file.fileType)(file.name))
 	}
 	return nil
 }
 
 // 读取目录
 func readDir(path string, level int) ([]FileTree, error) {
-	var list []FileTree
+	var files []FileTree
 	dir, err := ioutil.ReadDir(path)
 	if err != nil {
-		return list, err
+		return files, err
 	}
 	for _, fi := range dir {
 		if fi.IsDir() {
-			list = append(list, FileTree{level, Folder, fi.Name()})
-			l, err := readDir(path+"/"+fi.Name(), level+1)
+			files = append(files, FileTree{level, Folder, fi.Name()})
+			fs, err := readDir(path+"/"+fi.Name(), level+1)
 			if err != nil {
-				return list, err
+				return files, err
 			}
-			list = append(list, l...)
+			files = append(files, fs...)
 		} else if strings.HasSuffix(fi.Name(), ".tar") {
-			list = append(list, FileTree{level, TarFile, fi.Name()})
+			files = append(files, FileTree{level, TarFile, fi.Name()})
 			tars, err := readTar(path+"/"+fi.Name(), level)
 			if err != nil {
-				return list, err
+				return files, err
 			}
-			list = append(list, tars...)
+			files = append(files, tars...)
 		} else {
-			list = append(list, FileTree{level, GeneralFile, fi.Name()})
+			files = append(files, FileTree{level, GeneralFile, fi.Name()})
 		}
 	}
-	return list, err
+	return files, err
 }
 
 // 读取tar文件
-func readTar(path string, level int) (list []FileTree, err error) {
+func readTar(path string, level int) ([]FileTree, error) {
+	var files []FileTree
 	reader, err := os.Open(path)
 	if err != nil {
-		return
+		return files, err
 	}
 	defer reader.Close()
 
 	tarReader := tar.NewReader(reader)
 	for {
-		header, tarErr := tarReader.Next()
-		if tarErr == io.EOF {
+		header, err := tarReader.Next()
+		if err == io.EOF {
 			break
-		} else if tarErr != nil {
-			err = tarErr
-			return
+		} else if err != nil {
+			return files, err
 		}
 
 		fileInfo := header.FileInfo()
@@ -138,9 +138,9 @@ func readTar(path string, level int) (list []FileTree, err error) {
 			fileType = Folder
 			lv = lv - 1
 		}
-		list = append(list, FileTree{lv, fileType, fileInfo.Name()})
+		files = append(files, FileTree{lv, fileType, fileInfo.Name()})
 	}
-	return
+	return files, err
 }
 
 // 打印对应的颜色
